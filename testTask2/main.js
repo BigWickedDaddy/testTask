958794ded00048d1fe5c9bcfa0883d25fb90863e
BigWickedDaddy <81323621+BigWickedDaddy@users.noreply.github.com>
2022-10-20 00:05:48 +0300

    isDone

---
+++ testTask2/main.js
@@ -0,0 +1,17 @@
+
+clck();
+function clck(){
+    document.getElementById("hide").click();
+}
+
+function show() {
+    var x = document.getElementById("content");
+    x.style.display = "block";
+}
+function hide() {
+    var x = document.getElementById("content");
+    x.style.display = "none";
+}
+function Al() {
+    alert("Done!");
+}
